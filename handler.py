d29d5cb48690ad95d9a444f7b436b93b12f37d1e
Jon Borgonia <jon.borgonia@gmail.com>
2020-03-17 11:12:35 -0700

    initial svg handler
    handles
    - text
    - color
    - bgColor

---
+++ handler.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+'''
+params:
+    - text     <multiline: ok> # not really yet
+    - color    <default: black>
+    - bgColor  <default: None>
+    - fontSize <default: 1.2em>
+    - fontFace <default: monospace>
+    - radius   <default: 5>
+
+returns:
+    svg document
+'''
+def toSvg(
+        text='',
+        color='black',
+        bgColor=None,
+        fontSize="1.2em",
+        font='monospace',
+        radius=3,
+    ):
+
+    width = (0.55 * len(text)) + .1
+
+    background = f'<rect fill="{bgColor}" width="100%" height="100%" rx="{radius}"/>' if bgColor is not None else ''
+
+    return f"""<?xml version="1.0" encoding="utf-8"?>
+        <svg version="1.1"
+             xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink"
+             width="{width}em" height="1em"
+        >
+          {background}
+          <text font-size="{fontSize}" font-family="{font}" x=".1em" y="1em">
+            <tspan fill="{color}">{text}</tspan>
+          </text>
+        </svg>"""
+
+print(toSvg(text='hello world'))
